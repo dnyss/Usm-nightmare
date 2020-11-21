c023554cec5961406f4b3377034955fecb4e8811
dnyss <denyssmc2001@gmail.com>
2020-11-21 14:22:18 -0300

    Menu actualizado
    Los botones de jugar, opciones, controles y salir ahora funcionan! :D

---
+++ Usm nightmare/Assets/Cosas menu/GoToMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToMenu : MonoBehaviour
+{
+    //Para el botón de jugar
+    public void GoTomenu()
+    {
+        SceneManager.LoadScene("Menu"); //Se cargará la escena del menu
+    }
+}
---
+++ Usm nightmare/Assets/Cosas menu/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Para el botón de jugar
+    public void EleccionPersonaje()
+    {
+        SceneManager.LoadScene("Personaje"); //Se cargará la escena de eleccion de personaje
+    }
+    //Para el botón de salir
+    public void CerrarJuego()
+    {
+        Application.Quit();
+    }
+    //Para el botón de controles y opciones estan en la función onClick de cada botón
+}
