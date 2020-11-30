5d2431bb448e3acc9d1a48eee79ad7f7230f409b
JavAlex17 <72239869+JavAlex17@users.noreply.github.com>
2020-11-30 19:22:31 -0300

    Seleccon personaje (incompleto)

---
+++ Usm nightmare/Assets/Cosas menu/Elegirpersona.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Elegirpersona: MonoBehaviour{
+    public void ElegirPers (){
+
+        SceneManager.LoadScene("Jugador");
+    
+    }  
+
+}
+public class boton1 : MonoBehaviour{
+    public GameObject Chica;
+        public void ElegirPers (){
+        void OnMouseDown()
+        {
+            if(Input.GetMouseButtonDown(0)){
+                Chica.SetActive (true); 
+            }
+        }
+
+        SceneManager.LoadScene("Jugador");
+    
+    }  
+
+
+
+}
+
+
+public class boton2 : MonoBehaviour{
+    public GameObject Chico;
+        public void ElegirPers (){
+        void OnMouseDown()
+        {
+            if(Input.GetMouseButtonDown(0)){
+                Chico.SetActive (true); 
+            }
+        }
+
+        SceneManager.LoadScene("Jugador");
+    
+    } 
+
+} 
