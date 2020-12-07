24852dccbb6c9b59f2cfc37f5e72d9a2307cfbfb
JavAlex17 <72239869+JavAlex17@users.noreply.github.com>
2020-12-07 00:14:37 -0300

    Barra de vida 1

---
+++ Usm nightmare/Assets/Barra.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Barra : MonoBehaviour
+{
+    public Image health;
+    float hp, maxHp = 100f;
+   
+    void Start(){
+        hp = maxHp;
+    }
+    
+    public void TakeDamage(float amount){
+        hp = Mathf.Clamp(hp-amount, 0f , maxHp);
+        health.transform.localScale = new Vector2(hp/maxHp, 1);
+   
+    } 
+
+
+}
