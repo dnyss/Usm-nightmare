9a04f35ca1ecfc964c20facf4e4c69be5ff88273
JavAlex17 <72239869+JavAlex17@users.noreply.github.com>
2020-12-10 00:20:29 -0300

    Revert "Update Enemy.cs"
    This reverts commit 9b1fc5b3c00549436612dfa5e0ee846d1398336e.

--- Usm nightmare/Assets/Enemy.cs
+++ Usm nightmare/Assets/Enemy.cs
@@ -30,9 +30,4 @@ public class Enemy : MonoBehaviour
         GetComponent<SpriteRenderer>().enabled = false;
 
     }
-    void OnTriggerEnter2D(Collider2D col){
-        if (col.gameObject.tag == "Player"){
-            col.SendMessage("EnemyKnockBack");
-        }
-    }
 }
