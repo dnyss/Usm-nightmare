f64ab17328eca4dba4d1d981e0041f90c7a3900b
JavAlex17 <72239869+JavAlex17@users.noreply.github.com>
2020-12-19 17:03:22 -0300

    scr Información

---
+++ Usm nightmare/Assets/scr_InfoPartida.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class scr_InfoPartida 
+{
+    public static class infoJugador {
+        public static Vector2 posicion;
+        public static int energia;
+    }
+
+}
