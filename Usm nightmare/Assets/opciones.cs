fd8e0050ad37962337482fd0a183759dea05118e
dnyss <denyssmc2001@gmail.com>
2020-12-23 18:05:34 -0300

    Detalles extra
    Adornos en los niveles, música de fondo

---
+++ Usm nightmare/Assets/opciones.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class opciones : MonoBehaviour
+{
+    public void PantallaCompleta (bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+}
